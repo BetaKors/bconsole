ed3e5d5783a0f816213413d85f329f1a8c5002a8
BetaKors <leparkorsz@gmail.com>
2024-11-09 10:54:34 -0300

    refactor, add Cursor and Erase classes and more Also added the `actions` method and renamed the `action` method to `arrow`.

--- bconsole/console.py
+++ bconsole/console.py
@@ -1,20 +1,20 @@
 # reference: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 
-import os
 from getpass import getpass
-from typing import Iterable, NoReturn, final, overload
+from os import system as execute
+from typing import Any, Literal, NoReturn, final, overload
 
 from colorama import just_fix_windows_console
+from unidecode import unidecode
 
 _ESCAPE = "\033"
-_is_windows = os.name == "nt"
 
 just_fix_windows_console()
 
 
 @final
 class Foreground:
-    """Foreground colors, as per some spec I don't know anything about."""
+    """Foreground colors."""
 
     BLACK = f"{_ESCAPE}[30m"
     RED = f"{_ESCAPE}[31m"
@@ -43,12 +43,21 @@ class Foreground:
 
     @staticmethod
     def make(code: int) -> str:
+        """
+        Creates an Escape Code Sequence for the foreground color using the ANSI Code provided.
+
+        ### Args:
+            code (int): code
+
+        ### Returns:
+            str: Escape Code Sequence
+        """
         return f"{_ESCAPE}[{code}m"
 
 
 @final
 class Background:
-    """Background colors, as per some spec someone very smart wrote."""
+    """Background colors."""
 
     BLACK = f"{_ESCAPE}[40m"
     RED = f"{_ESCAPE}[41m"
@@ -91,7 +100,7 @@ class Background:
 
 @final
 class Modifier:
-    """Modifiers, as per some spec. No clue which tho."""
+    """Color/Graphics modifiers."""
 
     RESET = f"{_ESCAPE}[0m"
     BOLD = f"{_ESCAPE}[1m"
@@ -107,6 +116,141 @@ class Modifier:
     STRIKETHROUGH = f"{_ESCAPE}[9m"
 
 
+@final
+class Cursor:
+    """Cursor movement codes."""
+
+    HOME = f"{_ESCAPE}[H"
+    UP = f"{_ESCAPE}[1A"
+    DOWN = f"{_ESCAPE}[1B"
+    RIGHT = f"{_ESCAPE}[1C"
+    LEFT = f"{_ESCAPE}[1D"
+
+    @staticmethod
+    def set(column: int, line: int) -> str:
+        return f"{_ESCAPE}[{line};{column}H"
+
+    @staticmethod
+    def up(lines: int = 1) -> str:
+        """
+        Moves cursor up by the number of lines provided.
+
+        ### Args:
+            lines (int, optional): Number of lines to move. Defaults to 1.
+
+        ### Returns:
+            str: Escape Code Sequence
+        """
+        return f"{_ESCAPE}[{lines}1A"
+
+    @staticmethod
+    def down(lines: int = 1) -> str:
+        """
+        Moves cursor down by the number of lines provided.
+
+        ### Args:
+            lines (int, optional): Number of lines to move. Defaults to 1.
+
+        ### Returns:
+            str: Escape Code Sequence
+        """
+        return f"{_ESCAPE}[{lines}1B"
+
+    @staticmethod
+    def right(columns: int = 1) -> str:
+        """
+        Moves cursor to the right by the number of columns provided.
+
+        ### Args:
+            columns (int, optional): Number of columns to move. Defaults to 1.
+
+        ### Returns:
+            str: Escape Code Sequence
+        """
+        return f"{_ESCAPE}[{columns}1C"
+
+    @staticmethod
+    def left(columns: int = 1) -> str:
+        """
+        Moves cursor to the left by the number of columns provided.
+
+        ### Args:
+            columns (int, optional): Number of columns to move. Defaults to 1.
+
+        ### Returns:
+            str: Escape Code Sequence
+        """
+        return f"{_ESCAPE}[{columns}1D"
+
+    @staticmethod
+    def save_pos(sequence: Literal["DEC", "SCO"] = "DEC") -> str:
+        """
+        Saves the current cursor position for use with restore_pos at a later time.
+
+        ### Note:
+        The escape sequences for "save cursor position" and "restore cursor position" were never standardised as part of
+        the ANSI (or subsequent) specs, resulting in two different sequences known in some circles as "DEC" and "SCO":
+            DEC: ESC7 (save) and ESC8 (restore)
+            SCO: ESC[s (save) and ESC[u (restore)
+
+        Different terminals (and OSes) support different combinations of these sequences (one, the other, neither or both);
+        for example the iTerm2 terminal on macOS supports both, while the built-in macOS Terminal.app only supports the DEC sequences.
+
+        Source:
+            https://github.com/fusesource/jansi/issues/226
+            https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797#:~:text=saved%20position%20(SCO)-,Note,-%3A%20Some%20sequences
+
+        ### Args:
+            sequence (Literal["DEC", "SCO"], optional): which sequence to use. Defaults to "DEC".
+
+        ### Returns:
+            str: Escape Code Sequence
+        """
+        return f"{_ESCAPE}7" if sequence == "DEC" else f"{_ESCAPE}[s"
+
+    @staticmethod
+    def restore_pos(sequence: Literal["DEC", "SCO"] = "DEC") -> str:
+        """
+        Restores the current cursor position, which was previously saved with save_pos.
+
+        ### Note:
+        The escape sequences for "save cursor position" and "restore cursor position" were never standardised as part of
+        the ANSI (or subsequent) specs, resulting in two different sequences known in some circles as "DEC" and "SCO":
+            DEC: ESC7 (save) and ESC8 (restore)
+            SCO: ESC[s (save) and ESC[u (restore)
+
+        Different terminals (and OSes) support different combinations of these sequences (one, the other, neither or both);
+        for example the iTerm2 terminal on macOS supports both, while the built-in macOS Terminal.app only supports the DEC sequences.
+
+        Source:
+            https://github.com/fusesource/jansi/issues/226
+            https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797#:~:text=saved%20position%20(SCO)-,Note,-%3A%20Some%20sequences
+
+        ### Args:
+            sequence (Literal["DEC", "SCO"], optional): which sequence to use. Defaults to "DEC".
+
+        ### Returns:
+            str: Escape Code Sequence
+        """
+        return f"{_ESCAPE}8" if sequence == "DEC" else f"{_ESCAPE}[u"
+
+
+@final
+class Erase:
+    """Erase codes."""
+
+    CURSOR_TO_END = f"{_ESCAPE}[0J"
+    CURSOR_TO_ENDL = f"{_ESCAPE}[0K"
+    START_TO_CURSOR = f"{_ESCAPE}[1K"
+    START_TO_END = f"{_ESCAPE}[1J"
+    SCREEN = f"{_ESCAPE}[2J"
+    LINE = f"{_ESCAPE}[2K"
+
+    @staticmethod
+    def lines(count: int = 1, /) -> list[str]:
+        return [Cursor.UP + Erase.LINE for _ in range(count)]
+
+
 class Console:
     """A simple class to make console output easier and more consistent!"""
 
@@ -120,6 +264,7 @@ class Console:
         error_color: str,
         hint_color: str,
         panic_color: str,
+        arrow: str,
     ) -> None: ...
 
     @overload
@@ -136,20 +281,21 @@ class Console:
         self._panic_color: str = kwargs.get(
             "panic_color", Foreground.RED + Modifier.BOLD
         )
+        self._arrow = kwargs.get("arrow", ">>")
 
     def print(
         self,
-        text: str,
+        text: Any,
         color: str = Modifier.RESET,
         /,
         *,
+        sep: str = " ",
         end: str = "\n",
         flush: bool = False,
-        sep: str = " ",
         newline: bool = True,
     ) -> None:
         print(
-            f"{color}{text}{Modifier.RESET}",
+            f"{color}{str(text)}{Modifier.RESET}",
             end=end if newline else " ",
             flush=flush,
             sep=sep,
@@ -165,27 +311,26 @@ class Console:
         is_password: bool = False,
     ) -> str:
         self.print(prompt, self._prompt_color)
-        self.print(">>", self._arrow_color, newline=False, flush=True)
+        self.arrow(flush=True)
 
         res = (getpass if is_password else input)("").strip()
-
-        if res == "cls" or res == "clear":
-            self.clear()
-            return self.input(prompt)
-
-        if res == "exit":
-            exit(0)
 
         invalid_values = invalid_values or []
 
         if ensure_not_empty:
             invalid_values.append("")
 
-        if res in invalid_values:
-            self.error("Invalid value. Try again.")
-            return self.input(prompt, invalid_values=invalid_values)
-
-        return res
+        match res:
+            case "cls" | "clear":
+                self.clear()
+                return self.input(prompt)
+            case "exit":
+                exit(0)
+            case res if res in invalid_values:
+                self.error("Invalid value. Try again.")
+                return self.input(prompt, invalid_values=invalid_values)
+            case _:
+                return res
 
     def options(
         self,
@@ -193,38 +338,49 @@ class Console:
         /,
         *,
         options: list[str] | None = None,
-        capitalize: bool = True,
+        wrapper: str | None = "[]",
+        title: bool = True,
+        format: bool = True,
     ) -> str:
-        options = options or ["yes", "no"]
+        options = options or ["Yes", "No"]
+        wrapper = wrapper or ""
 
-        if self._has_repetitions(opt[0].lower() for opt in options):
-            raise ValueError(
-                "Options can't have two options whose first letter is the same."
-            )
+        simplified_options = list(map(lambda o: unidecode(o).lower(), options))
 
-        kv_options = {option[0].lower(): option for option in options}
+        formatted_options = self.items(
+            *[
+                self._surround(option, wrapper, title) if format else option
+                for option in options
+            ]
+        )
 
         while True:
-            option = self.input(
-                f'{prompt} {self._format_iter(f"[{opt[0].upper() if capitalize else opt[0]}]{opt[1:].lower()}" for opt in options)}'
-            ).lower()[0]
+            chosen = unidecode(self.input(f"{prompt} {formatted_options}.")).lower()
 
-            if option in kv_options.keys():
+            filtered = [
+                option for option in simplified_options if option.startswith(chosen)
+            ]
+
+            if len(filtered) == 1:
                 self.erase_lines()
-                self.action(f"Chosen option: {kv_options[option]}", Foreground.MAGENTA)
-                return option
+                self.arrow(f"Chosen option: {filtered[0]}", Foreground.MAGENTA)
+                return chosen
 
             self.error(
                 "Invalid option.",
-                hint=f"Choose one among the following options: {self._format_iter(kv_options.keys(), upper=True)}",
+                hint=f"Choose one among the following options: {formatted_options}.",
             )
 
-    def action(self, text: str, color: str = Modifier.RESET, /) -> None:
-        self.print(">>", self._arrow_color, newline=False)
-        self.print(text, color)
+    def items(
+        self,
+        *items: Any,
+        sep: str = ", ",
+        final_sep: str = " or ",
+    ) -> str:
+        return self._reverse_replace(sep.join(map(str, items)), sep, final_sep)
 
     def error(self, error: Exception | str, /, *, hint: str = "") -> None:
-        self.print(str(error), self._error_color)
+        self.print(error, self._error_color)
         _ = hint and self.print(hint, self._hint_color)
 
     def panic(self, error: str, /, *, hint: str = "", code: int = -1) -> NoReturn:
@@ -232,26 +388,33 @@ class Console:
         self.enter_to_continue()
         exit(code)
 
+    def arrow(
+        self, text: str = "", color: str = Modifier.RESET, /, *, flush: bool = False
+    ) -> None:
+        self.print(self._arrow, self._arrow_color, newline=False, flush=flush)
+        _ = text and self.print(text, color)
+
+    def actions(self, *args: str) -> None:
+        self.print("\n".join(args), end="")
+
     def enter_to_continue(self, text: str = "Press enter to continue...") -> None:
         self.input(text, ensure_not_empty=False, is_password=True)
         self.erase_lines(2)
 
-    def clear(self):
-        os.system("cls" if _is_windows else "clear")
-
     def erase_lines(self, count: int = 1, /) -> None:
-        cursor_up_one = f"{_ESCAPE}[1A"
-        erase_line = f"{_ESCAPE}[2K"
+        self.actions(*Erase.lines(count))
 
-        for _ in range(count):
-            print(cursor_up_one + erase_line, end="", flush=True)
+    def clear(self):
+        execute("cls||clear")
 
-    def _format_iter(self, iter: Iterable[str], upper: bool = False) -> str:
-        _iter = (txt.upper() if upper else txt for txt in iter)
-        return self._reverse_replace(", ".join(_iter), ", ", " or ") + "."
+    def _surround(self, text: str, wrapper: str, title: bool = True) -> str:
+        w1, w2 = self._cut_at(wrapper)
+        return f"{w1}{text.title() if title else text}{w2}"
+
+    def _cut_at(self, text: str, t: float = 0.5) -> tuple[str, str]:
+        size = len(text)
+        where = round(size * t)
+        return (text[:where], text[where:])
 
     def _reverse_replace(self, text: str, old: str, new: str) -> str:
         return new.join(text.rsplit(old, 1))
-
-    def _has_repetitions(self, iter: Iterable[str]) -> bool:
-        return len(lst := list(iter)) != len(set(lst))
--- bconsole/examples/example.py
+++ bconsole/examples/example.py
@@ -1,19 +1,26 @@
-from bconsole import Console, Foreground
+from bconsole import Console, Foreground, Modifier
 
 console = Console()
 
-console.print("Hello World!", Foreground.make_rgb(255, 128, 30))
-console.input("What is your name?")
-console.options("Do you like the color red?")  # defaults to yes/no
+console.print("Hello World!", Foreground.WHITE + Modifier.BOLD)
+console.print(
+    "You can make completely custom colors too!",
+    Foreground.make_rgb(255, 128, 30),  # not supported in all terminals
+)
+name = console.input("What is your name?")
+
+console.print(f"Hello, {name}!", Foreground.make(31))  # same as Foreground.RED
+
+console.options("Do you like the color red?")  # defaults to Yes/No
 console.options("Which animal do you prefer, cats or dogs?", options=["cats", "dogs"])
 
-try:
-    console.options(
-        "Choose a programming language: Python or PHP", options=["Python", "PHP"]
-    )
-except ValueError as e:
-    console.error(e)
+for i in range(3):
+    console.print(f"This is line number {i + 1}.")
 
-console.action("I'm doing something important!")
+console.erase_lines(2)
+
+console.arrow("I just did something important!")
+
 console.error("Something went wrong!")
-console.panic("Something went really wrong!", hint="Maybe you should try again?")
+console.panic("Something went really wrong!", code=1)
+console.print("This will not be printed.")
